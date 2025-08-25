123155f4af41cd3cf1057488077fee47ebf80ac8
Paulo Rezeck <rezeck@dcc.ufmg.br>
2025-08-25 10:15:49 -0300

    Update LiDAR configuration for LSLidar C32W
    - Changed point cloud topic from "/mid/points" to "/cx/lslidar_point_cloud".
    - Updated useCloudRing to true for compatibility with LSLidar C32W.
    - Adjusted N_SCAN to 32 and modified angular resolution parameters accordingly.

--- LeGO-LOAM/include/utility.h
+++ LeGO-LOAM/include/utility.h
@@ -57,16 +57,24 @@ extern const string imuTopic = "/imu/imu_and_mag";
 // Save pcd
 extern const string fileDirectory = "/tmp/";
 
-// Using velodyne cloud "ring" channel for image projection (other lidar may have different name for this channel, change "PointXYZIR" below)
-extern const bool useCloudRing = false; // if true, ang_res_y and ang_bottom are not used
+// Using lidar "ring" channel for image projection (LSLidar C32W has ring field)
+extern const bool useCloudRing = true; // if true, ang_res_y and ang_bottom are not used
 
 // VLP-16
-extern const int N_SCAN = 16;
+// extern const int N_SCAN = 16;
+// extern const int Horizon_SCAN = 1800;
+// extern const float ang_res_x = 0.2;
+// extern const float ang_res_y = 2.0;
+// extern const float ang_bottom = 15.0+0.1;
+// extern const int groundScanInd = 7;
+
+// LSLidar C32W
+extern const int N_SCAN = 32;
 extern const int Horizon_SCAN = 1800;
 extern const float ang_res_x = 0.2;
-extern const float ang_res_y = 2.0;
-extern const float ang_bottom = 15.0+0.1;
-extern const int groundScanInd = 7;
+extern const float ang_res_y = 70.0/float(N_SCAN-1);  // 70° FOV / 31 intervals = ~2.26°
+extern const float ang_bottom = 55.0;  // bottom beam at -55°, so offset is 55°
+extern const int groundScanInd = 20;
 
 // HDL-32E
 // extern const int N_SCAN = 32;
